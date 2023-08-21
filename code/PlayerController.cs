a05174a4ed732c647b3f3bb7368c87edbfa891e3
Cody <musoudeus@gmail.com>
2023-08-21 15:50:35 +0800

    use the animation method supplied by Unity to make the walking animation,we also add a little paramater in cs file to make the animation more realistic.For example,we let the speed of the animation relate to the speedx of the object(which is completed by last state)

--- code/PlayerController.cs
+++ code/PlayerController.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour{
+
+    // 動畫
+    Animator animator;
+
+    // 物理
     Rigidbody2D rigid2D;
     float jumpForce = 340.0f;
     float walkForce = 30.0f;
     float maxWalkSpeed = 2.0f;
     
     void Start(){
-        this.rigid2D = GetComponent<Rigidbody2D>();
+        this.rigid2D = GetComponent<Rigidbody2D>(); // 物理
+        this.animator = GetComponent<Animator>(); // 動畫
     }
 
     void Update(){
@@ -37,5 +43,9 @@ public class PlayerController : MonoBehaviour{
         if(key != 0){
             transform.localScale = new Vector3(key,1,1); // 翻轉方式->往-x軸方向擴大
         }
+
+
+        // 依角色變化改變動畫的速度
+        this.animator.speed = speedx / 2.0f; // 動畫速度會是移動速度的0.5倍(real time with the current speed)
     }
 }
