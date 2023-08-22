c733f7043e95cdeee07aa8e7b05910fd1b803ea7
Cody <musoudeus@gmail.com>
2023-08-22 13:57:22 +0800

    adjust scene reaction to the player when he touch the flag

--- code/PlayerController.cs
+++ code/PlayerController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour{
 
     // 動畫
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour{
 
     // 物理
     Rigidbody2D rigid2D;
-    float jumpForce = 340.0f;
+    float jumpForce = 680.0f;
     float walkForce = 6.0f;
     float maxWalkSpeed = 0.9f;
     
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour{
 
     void Update(){
         // 跳躍
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if(Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0){
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
 
@@ -47,10 +47,16 @@ public class PlayerController : MonoBehaviour{
 
         // 依角色變化改變動畫的速度
         this.animator.speed = speedx / 2.0f; // 動畫速度會是移動速度的0.5倍(real time with the current speed)
+
+        // 跑出畫面時回到初始畫面
+        if(transform.position.y < -10 ){
+            SceneManager.LoadScene("GameScene");
+        }
         
     }
     // 抵達終點
     void OnTriggerEnter2D(Collider2D other){
         Debug.Log("終點");
+        SceneManager.LoadScene("ClearScene");
     }
 }
