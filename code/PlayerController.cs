27d69a67cfc29a1bbfef4c774abb4d4ab81258ca
Cody <musoudeus@gmail.com>
2023-08-23 12:02:43 +0800

    adjust the GetKeyDown function because no device avaliable so just use pc to play it

--- code/PlayerController.cs
+++ code/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : MonoBehaviour{
     float jumpForce = 680.0f;
     float walkForce = 6.0f;
     float maxWalkSpeed = 0.9f;
-    float threshold = 0.2f;
+    // float threshold = 0.2f;
 
     void Start(){
         this.rigid2D = GetComponent<Rigidbody2D>(); // 物理
@@ -21,14 +21,15 @@ public class PlayerController : MonoBehaviour{
 
     void Update(){
         // 跳躍
-        if(Input.GetMouseButtonDown(0) && this.rigid2D.velocity.y == 0){
+        if(Input.GetKey(KeyCode.Space) && this.rigid2D.velocity.y == 0){
+            this.animator.SetTrigger("JumpTrigger");
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
 
         // 左右移動
         int key = 0;
-        if(Input.acceleration.x > this.threshold) key = 1;
-        if(Input.acceleration.x < -this.threshold) key = -1;
+        if(Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if(Input.GetKey(KeyCode.LeftArrow)) key = -1;
         // 遊戲角色的速度
         float speedx = Mathf.Abs(this.rigid2D.velocity.x);
 
@@ -45,6 +46,14 @@ public class PlayerController : MonoBehaviour{
 
         // 依角色變化改變動畫的速度
         this.animator.speed = speedx / 2.0f; // 動畫速度會是移動速度的0.5倍(real time with the current speed)
+
+        // 依照遊戲角色速度改變動畫速度
+        if(this.rigid2D.velocity.y == 0){
+            this.animator.speed = speedx / 2.0f;
+        } else {
+            this.animator.speed = 1.0f;
+        }
+
 
         // 跑出畫面時回到初始畫面
         if(transform.position.y < -10 ){
